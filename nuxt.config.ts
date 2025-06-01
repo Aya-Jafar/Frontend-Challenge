4c2aefbc5919e649a2d0d5e925a0731770e8f230
AyaJafar <aya.jafar002@gmail.com>
2025-06-01 20:15:03 +0300

    Initial config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: false },
+  srcDir: "src/",
+  components: true,
+  css: ["~/assets/css/app.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
---
+++ src/components/hello.vue
@@ -0,0 +1,13 @@
+<script setup>
+const { data } = await useFetch('/api/hello'); 
+const message = computed(() => data?.value?.message ?? "Loading...");
+</script>
+
+<template>
+  <div>
+    HII
+    <h1 class="text-red-900">{{ message }}</h1>
+  </div>
+</template>
+
+
